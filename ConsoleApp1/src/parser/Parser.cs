be506c2c968c40f019768ab14b8a9247add82331
tnechepurencko <321avisam5@gmail.com>
2024-02-12 12:36:13 +0300

    while

--- ConsoleApp1/src/parser/Parser.cs
+++ ConsoleApp1/src/parser/Parser.cs
@@ -128,14 +128,12 @@ public class Parser
     {
         if (stmt.TryGetProperty("D", out JsonElement decl))
         {
-            // declaration processing
             ParseDecl(decl, md, proc);
             return;
         }
         
         if (stmt.TryGetProperty("L", out JsonElement lAssig) && stmt.TryGetProperty("R", out JsonElement rAssig))
         {
-            // assignment processing
             ParseAssignment(lAssig, rAssig, proc);
             return;
         }
@@ -144,11 +142,61 @@ public class Parser
             stmt.TryGetProperty("Else", out JsonElement els))
         {
 	        ParseIfElse(cond, then, els, md, proc);
+	        return;
+        }
+        
+        if (stmt.TryGetProperty("Cond", out JsonElement wCond) && stmt.TryGetProperty("Seq", out JsonElement seq))
+        {
+	        ParseWhile(wCond, seq, md, proc);
         }
         
         Print("no");
     }
 
+    public void ParseWhile(JsonElement cond, JsonElement seq, MethodDefinition md, ILProcessor proc)
+    {
+	    var condDef = new VariableDefinition(_asm.MainModule.TypeSystem.Boolean);
+	    md.Body.Variables.Add(condDef);
+	    GenerateCondition(cond, proc, condDef);
+	    
+	    // int i = 0;
+	    var var_i = new VariableDefinition(_asm.MainModule.TypeSystem.Int32);
+	    md.Body.Variables.Add(var_i);
+	    proc.Emit(OpCodes.Ldc_I4, 0);
+	    proc.Emit(OpCodes.Stloc, var_i);
+
+	    var lblFel = proc.Create(OpCodes.Nop);
+	    var nop = proc.Create(OpCodes.Nop);
+	    proc.Append(nop);
+	    
+	    proc.Emit(OpCodes.Ldloc, condDef); // while condDef is not false
+	    proc.Emit(OpCodes.Brfalse, lblFel);
+	    
+	    var statements = seq.GetProperty("Statements"); // arr
+	    for (int i = 0; i < statements.GetArrayLength(); i++)
+	    {
+		    GenerateCondition(cond, proc, condDef);
+		    GenerateStatement(statements[i], null, proc, md);
+	    }
+	    
+	    proc.Emit(OpCodes.Ldloc, var_i);
+	    proc.Emit(OpCodes.Dup);
+	    proc.Emit(OpCodes.Ldc_I4_1);
+	    proc.Emit(OpCodes.Add);
+	    proc.Emit(OpCodes.Stloc, var_i);
+	    proc.Emit(OpCodes.Pop);
+	    proc.Emit(OpCodes.Br, nop);
+	    proc.Append(lblFel);
+    }
+    
+    public void GenerateCondition(JsonElement cond, ILProcessor proc, VariableDefinition condDef)
+    {
+	    string? type = cond.GetProperty("Typ").GetProperty("Name").GetString();
+	    if (type != null) GenerateOperation(cond, type, proc);
+	    proc.Emit(OpCodes.Stloc, condDef);
+    }
+
+    // todo check whether elif exists
     public void ParseIfElse(JsonElement cond, JsonElement then, JsonElement? els, MethodDefinition md, ILProcessor proc)
     {
 	    string? type = cond.GetProperty("Typ").GetProperty("Name").GetString();
